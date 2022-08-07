b77871393e4ba9596571622569ad61247e7000ca
c.giacometti <camilla.giacometti@gmail.com>
2022-08-07 18:43:12 -0300

    correções

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,15 +6,6 @@ export async function getUser(req, res){
 
         const userId = res.locals.userId;
 
-        const { rows: userData } = await connection.query(
-            `SELECT users.id, users.name, SUM(urls."visitCount") AS "visitCount"
-            FROM users
-            JOIN urls on users.id= urls."userId"
-            WHERE users.id= $1
-            GROUP BY users.id`,
-            [userId]
-        );
-
         const { rows: userExists } = await connection.query(
             `SELECT * FROM users
             WHERE id= $1`,
@@ -25,6 +16,15 @@ export async function getUser(req, res){
             return res.status(404).send("User not found");
         }
 
+        const { rows: userData } = await connection.query(
+            `SELECT users.id, users.name, SUM(urls."visitCount") AS "visitCount"
+            FROM users
+            JOIN urls on users.id= urls."userId"
+            WHERE users.id= $1
+            GROUP BY users.id`,
+            [userId]
+        );
+
         const { rows: shortenedUrls } = await connection.query(
             `SELECT id, "shortUrl", url, "visitCount"
             FROM urls
@@ -32,8 +32,17 @@ export async function getUser(req, res){
             [userId]
         );
 
-        const [ user ] = userData;
-        const { id, name, visitCount } = user;
+        if(userData.length > 0){
+            const [ user ] = userData;
+            const { id, name, visitCount } = user;
+            const returnData = {id, name, visitCount, shortenedUrls};
+
+            return res.status(200).send(returnData);
+        }
+
+        const [ user ] = userExists;
+        const { id, name } = user;
+        const visitCount = 0;
         const returnData = {id, name, visitCount, shortenedUrls};
 
         res.status(200).send(returnData);
